bb74a89806de68fbf61fe1383751f2deee1a5e8e
Drguezpro <rodriguez.ottonello@gmail.com>
2024-03-07 14:44:29 -0300

    Realizamos la github page

--- Proyecto/server.js
+++ Proyecto/server.js
@@ -17,5 +17,14 @@ app.listen(PORT, '192.168.0.xxx', () => {
     console.log(`Servidor en ejecución en el puerto ${PORT}`);
 });
 
+// Capturar la señal de interrupción (Ctrl + C)
+process.on('SIGINT', () => {
+    console.log('Apagando el servidor...');
+    // Aquí puedes realizar acciones adicionales, como cerrar conexiones de base de datos.
+    // Por ejemplo:
+    // mongoose.disconnect(); // Si usas Mongoose para MongoDB
+    process.exit(); // Detiene el proceso de Node.js
+});
+
 
 
